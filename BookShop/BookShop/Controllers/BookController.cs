d90d7766e8043e676fd72e4f05f8e005f2abd0d1
Pavzar <pavelzarov@yahoo.com>
2019-12-15 18:54:54 +0200

    Added Controllers folder with BookController, configured bookcontroller to return viewresult of books. Also added Views folder with additional folder Book, containing razor view file List

---
+++ BookShop/BookShop/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookShop.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public BookController(IBookRepository bookRepository, ICategoryRepository categoryRepository)
+        {
+            _bookRepository = bookRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            return View(_bookRepository.AllBooks);// displays all books
+        }
+    }
+}
